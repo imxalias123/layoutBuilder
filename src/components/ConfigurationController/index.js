353082686e14fb427c7934f9d7ae63eb24d11433
sailaxmi <sailaxmikomirineni@gmail.com>
2023-09-15 16:18:34 +0000

    solution

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -5,18 +5,24 @@ const ConfigurationController = () => (
   <div className="nav-container">
     <h1 className="nav-h1">Layout</h1>
     <div className="checkboxes-container">
-      <input id="checkbox" type="checkbox" className="checkbox" />
-      <label htmlFor="checkbox" className="label">
-        Content
-      </label>
-      <input id="checkbox" />
-      <label htmlFor="checkbox" className="label">
-        Left Navbar
-      </label>
-      <input id="checkbox" type="checkbox" className="checkbox" />
-      <label htmlFor="checkbox" className="label">
-        Right Navbar
-      </label>
+      <div className="spacing">
+        <input id="content" type="checkbox" className="checkbox" />
+        <label htmlFor="checkbox" className="label">
+          Content
+        </label>
+      </div>
+      <div className="spacing">
+        <input id="left-navbar" type="checkbox" className="checkbox" />
+        <label htmlFor="checkbox" className="label">
+          Left Navbar
+        </label>
+      </div>
+      <div className="spacing">
+        <input id="right-navbar" type="checkbox" className="checkbox" />
+        <label htmlFor="checkbox" className="label">
+          Right Navbar
+        </label>
+      </div>
     </div>
   </div>
 )
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,1 +1,48 @@
 // Write your code here
+import './index.css'
+
+const Layout = () => (
+  <div className="layout-container">
+    <div className="card-container">
+      <div className="header-container">
+        <h1 className="header-h1">Header</h1>
+      </div>
+      <div className="middle-container">
+        <div className="left-nav-bar-container">
+          <p className="left-nav-h1">Left Navbar Menu</p>
+          <div className="flex-wrap">
+            <p>Item 1</p>
+            <p>Item 2</p>
+            <p>Item 3</p>
+            <p>Item 4</p>
+          </div>
+        </div>
+        <div className="Content-container">
+          <p className="content-h1">Content</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim
+            ad minim venim.
+          </p>
+        </div>
+        <div className="right-nav-container">
+          <p className="left-nav-h1">Right Navbar</p>
+          <div className="wrap-box">
+            <div className="box">
+              <p>Ad 1</p>
+            </div>
+            <div className="box">
+              <p>Ad 2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-container">
+        <h1 className="footer-h1">Footer</h1>
+      </div>
+    </div>
+  </div>
+)
+
+export default Layout
